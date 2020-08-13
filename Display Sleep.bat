213f36f96aef77fe611c84414003331359d00ddb
Carmelo Santana <me@carmelosantana.com>
2020-08-13 13:13:33 -0400

    🚀

---
+++ Display Sleep.bat
@@ -0,0 +1,9 @@
+echo off
+
+C:\Windows\System32\powercfg.exe -Change -monitor-timeout-dc 1
+C:\Windows\System32\powercfg.exe -Change -monitor-timeout-ac 1
+
+START /MIN /WAIT C:\Windows\System32\scrnsave.scr -s
+
+C:\Windows\System32\powercfg.exe -Change -monitor-timeout-dc 30
+C:\Windows\System32\powercfg.exe -Change -monitor-timeout-ac 30
